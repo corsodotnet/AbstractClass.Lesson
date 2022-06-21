62cb368d864f2c96676def91c70113c812e2f12c
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-21 12:40:31 +0200

    Add project files.

---
+++ AbstractClass.Lesson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AbstractClass.Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Continente continente = new Continente();
+            Console.WriteLine("Il varole del padre +: "+ continente.NPositivi); 
+        }
+    }
+
+    public class AreaGeografica
+    {  
+        // membri
+        public int _nPositivi = 10 ;
+        public int _posizione;
+        public int _nAbitanti;
+
+    }
+    public class Continente : AreaGeografica
+    {
+        // public int _nPositivi = 10 ;
+           public new int _nPositivi = 20;
+           public int NPositivi { get { return base._nPositivi; } } 
+    }
+}
