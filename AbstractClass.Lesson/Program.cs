e13a251291a22337711297c42a9b18a94625f84a
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-21 16:19:14 +0200

    changes

--- AbstractClass.Lesson/Program.cs
+++ AbstractClass.Lesson/Program.cs
@@ -6,8 +6,9 @@ namespace AbstractClass.Lesson
     {
         static void Main(string[] args)
         {
-            Continente continente = new Continente();
-            Console.WriteLine("Il varole del padre +: "+ continente.NPositivi); 
+            Continente continente = new Continente();                       
+            Worker paolo = new Worker();            
+            Manager bruno = new Manager(paolo); 
         }
     }
 
@@ -21,8 +22,57 @@ namespace AbstractClass.Lesson
     }
     public class Continente : AreaGeografica
     {
-        // public int _nPositivi = 10 ;
-           public new int _nPositivi = 20;
-           public int NPositivi { get { return base._nPositivi; } } 
+
+           Country _germany = null;
+          // public int _nPositivi = 10 ;
+           public new int _nPositivi = 11;
+           public int NPositivi { get { return base._nPositivi; } }
+
+        public Continente()
+        {
+            _germany = new Country(); // Una proprietà di Contiente  
+        }
+
+    }
+    public class Country : AreaGeografica
+    {
+       
+    }
+    public class City : AreaGeografica
+    {
+
+    }
+
+    public class Manager
+    {
+        Worker _worker1 = null;
+
+        public Manager(Worker Worker)
+        {
+            _worker1 = Worker;
+        }
+    }
+    public class Worker
+    {
+
+    }
+
+    public class CoffeeMachine
+    {
+        public CoffeeMachine(Coffee caffe)
+        {
+
+        }
+
+        public void MakeCoffee(Coffee caffe) // Stack  -> oggetti temporane + Metodi e funzioni
+        {    
+            // fa il caffe 
+            // Esce dal ciclo 
+            // rilasdciato dalla memoria 
+        }
+    }
+    public class Coffee
+    {
+
     }
 }
